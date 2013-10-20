c7af4773ee6489d632333682a3655b4f1dd232f5
ernesto <bossi.ernestog@gmail.com>
2013-10-20 17:16:48 -0300

    added tests to testDiagram

--- test/testDiagram.py
+++ test/testDiagram.py
@@ -4,11 +4,20 @@ from TestingClassesRepo import *
 
 
 class TestDiagram(TestCase):
-
     def setUp(self):
         self.diagram = YUMLDiagram()
 
-    def testSettingClasses(self):
+    def test_setting_classes(self):
         self.diagram.add_class(CompleteClassTest)
         self.assertEqual(1, self.diagram.classes.__len__())
-        print self.diagram.classes[0].convert_to_service()
+        self.assertEqual('[CompleteClassTest|a;b;j|anotherMethod();method1();methodWithArgument()]',
+                         self.diagram.classes[0].convert_to_service())
+
+    def test_setting_atrtribute_only_class(self):
+        self.diagram.add_class(EmptyClassTest)
+        self.assertEqual(1, self.diagram.classes.__len__())
+        self.assertEqual('[EmptyClassTest|]', self.diagram.classes[0].convert_to_service())
+
+    def test_setting_empty_class(self):
+        self.diagram.add_class(AttributeOnlyClassTest)
+        self.assertEqual('[AttributeOnlyClassTest|attribute1;attribute2]', self.diagram.classes[0].convert_to_service())
